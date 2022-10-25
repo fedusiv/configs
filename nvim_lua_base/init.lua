15f23b7005181db9e60aea14626ab855e7e2eaa0
fedusiv <fedusivilya@gmail.com>
2022-10-25 23:50:45 +0300

    Remmaped tabs

--- nvim_lua_base/init.lua
+++ nvim_lua_base/init.lua
@@ -7,6 +7,8 @@ require('plugins')
 
 -- Setting map leader
 vim.g.mapleader = ' '
+-- Using only one copy/paste register and make it alligned with clipboard
+vim.cmd('set clipboard=unnamed')
 
 -- Hightlight number line
 vim.cmd('set number')
@@ -26,16 +28,18 @@ vim.opt.hlsearch = false
 -- Additional space before numbers.
 vim.wo.signcolumn = "yes"
 
+vim.cmd('set tabstop=4 softtabstop=0 expandtab shiftwidth=4 smarttab')
+
 -- Key remapping
 local function map(m, k ,v)
 	vim.keymap.set(m,k,v, {silent=true})
 end
-
 map('n', 'vv', '<c-v>') -- Enter visual block mode
 map('n', '<S-l>', '$') -- Move cursor to the end of line
 map('n', '<S-h>', '^') -- Move cursor to the start of line
 map('n', '<A-j>', ':m .+1<CR>==') -- Move whole line down
 map('n', '<A-k>', ':m-2<CR>==') -- Move whole line up above
+
 
 -- Telescope control
 map('n', '<leader>fe', ':Telescope find_files<cr>')
@@ -63,16 +67,16 @@ require'lualine'.setup()
 
 -- Buffer manipulation
 require'bufferline'.setup()
-map('n', '<A-1>', ':BufferGoto 1<cr>')
-map('n', '<A-2>', ':BufferGoto 2<cr>')
-map('n', '<A-3>', ':BufferGoto 3<cr>')
-map('n', '<A-4>', ':BufferGoto 4<cr>')
-map('n', '<A-5>', ':BufferGoto 5<cr>')
-map('n', '<A-6>', ':BufferGoto 6<cr>')
-map('n', '<A-7>', ':BufferGoto 7<cr>')
-map('n', '<A-8>', ':BufferGoto 8<cr>')
-map('n', '<A-9>', ':BufferGoto 9<cr>')
-map('n', '<A-0>', ':BufferLast<cr>')
-map('n', '<A-e>', ':BufferNext<cr>')
-map('n', '<A-w>', ':BufferPrevious<cr>')
-map('n', '<A-q>', ':BufferClose<cr>')
+map('n', '<leader>1', ':BufferGoto 1<cr>')
+map('n', '<leader>2', ':BufferGoto 2<cr>')
+map('n', '<leader>3', ':BufferGoto 3<cr>')
+map('n', '<leader>4', ':BufferGoto 4<cr>')
+map('n', '<leader>5', ':BufferGoto 5<cr>')
+map('n', '<leader>6', ':BufferGoto 6<cr>')
+map('n', '<leader>7', ':BufferGoto 7<cr>')
+map('n', '<leader>8', ':BufferGoto 8<cr>')
+map('n', '<leader>9', ':BufferGoto 9<cr>')
+map('n', '<leader>0', ':BufferLast<cr>')
+map('n', '<leader>e', ':BufferNext<cr>')
+map('n', '<leader>w', ':BufferPrevious<cr>')
+map('n', '<leader>q', ':BufferClose<cr>')
